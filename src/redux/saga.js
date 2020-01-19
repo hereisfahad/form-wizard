bf5f4a6922923c83d6eea963bd10480a0730f17b
Fahad Jabbar <fahadjabbar31@gmail.com>
2020-01-19 20:34:12 +0500

    Update saga.js

--- src/redux/saga.js
+++ src/redux/saga.js
@@ -8,9 +8,9 @@ const state = select();
 export function* apiCall(s) {
     yield console.log('api call')
     // yield console.log(state)
-    const data = ""
+    
     try {
-        const response = yield fetch("https://apiformwizard.herokuapp.com/register", {
+        let response = yield fetch("https://apiformwizard.herokuapp.com/register", {
             method: "post",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
@@ -19,7 +19,7 @@ export function* apiCall(s) {
         }
         );
 
-        data = yield response.json();
+       let data = yield response.json();
         yield put({ type: "USER_SAVED", data });
     }
     catch (error) {
