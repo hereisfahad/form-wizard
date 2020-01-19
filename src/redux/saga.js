ee5e4f5b62c5882ef663db449f2e8baf23232101
Fahad Jabbar <fahadjabbar31@gmail.com>
2020-01-19 21:50:50 +0500

    Update saga.js

--- src/redux/saga.js
+++ src/redux/saga.js
@@ -9,16 +9,14 @@ export function* apiCall(s) {
     yield console.log('api call')
     yield console.log(s.payload)//return state
     let d = {}
-    let abc = axios.post('https://apiformwizard.herokuapp.com/', s.payload)
-    abc.then(res => {
+    axios.post('https://apiformwizard.herokuapp.com/', s.payload)
+    .then(res => {
         console.log(res)
         d = res.data;
-
-    }
-    )
-        .catch((error) => {
-            console.log(error);
-        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
     yield put({ type: "USER_SAVED", d });
 
 }
